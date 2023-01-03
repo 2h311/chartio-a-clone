56ec013bc063b7fcac69aebfbac2f0306c4e4656
2h311 <kcodepal@outlook.com>
2023-01-03 13:54:24 -0600

    hello world. i think this is my first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+def mount_static(app):
+    app.mount("/assets", StaticFiles(directory="static"), name="static")
+
+
+def start_app():
+    app = FastAPI(title="Chartio Clone", version="this is a clone version")
+    mount_static(app)
+    return app
+
+
+app = start_app()
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("chartio/index.html", context={"request": request})
+
+

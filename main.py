5d3707c9d24c49d16791ee93ce3f2dc680393b3e
2h311 <kcodepal@outlook.com>
2023-03-13 09:57:18 -0500

    removed the routes from main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+
+from routes import router
 
 
 templates = Jinja2Templates(directory="templates")
@@ -18,36 +20,4 @@ def start_app():
 
 
 app = start_app()
-
-
-@app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse(
-        "chartio/index.html", context={"request": request}
-    )
-
-
-@app.get("/login")
-async def login(request: Request):
-    return templates.TemplateResponse(
-        "chartio/login.html", context={"request": request}
-    )
-
-
-@app.get("/users/password/reset")
-async def password_reset(request: Request):
-    return templates.TemplateResponse(
-        "chartio/password_reset.html", context={"request": request}
-    )
-
-
-@app.get("/users/password/reset/email-sent")
-async def password_reset(request: Request):
-    return templates.TemplateResponse(
-        "chartio/email_sent.html", context={"request": request}
-    )
-
-
-@app.get("/product/support")
-async def support(request: Request):
-    return templates.TemplateResponse("chartio/support.html", context={"request": request})
+app.include_router(router)
---
+++ routes.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Request
+
+
+router = APIRouter(include_in_schema=False)
+
+
+@router.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "chartio/index.html", context={"request": request}
+    )
+
+
+@router.get("/login")
+async def login(request: Request):
+    return templates.TemplateResponse(
+        "chartio/login.html", context={"request": request}
+    )
+
+
+@router.get("/users/password/reset")
+async def password_reset(request: Request):
+    return templates.TemplateResponse(
+        "chartio/password_reset.html", context={"request": request}
+    )
+
+
+@router.get("/users/password/reset/email-sent")
+async def password_reset(request: Request):
+    return templates.TemplateResponse(
+        "chartio/email_sent.html", context={"request": request}
+    )
+
+
+@router.get("/product/support")
+async def support(request: Request):
+    return templates.TemplateResponse(
+        "chartio/support.html", context={"request": request}
+    )

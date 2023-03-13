45e24dc600d9a349299538af31ba2f677e5776d8
2h311 <kcodepal@outlook.com>
2023-03-13 09:59:31 -0500

    made some corrections in routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
 
 
+templates = Jinja2Templates(directory="templates")
 router = APIRouter(include_in_schema=False)
 
 
